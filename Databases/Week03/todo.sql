e74e23dd793e8c1374a63af0509092ecb9cef65c
Darshana Sirisena <117305837+DarshanaSi@users.noreply.github.com>
2023-06-08 13:55:48 +0200

    Add

---
+++ Databases/Week03/todo.sql
@@ -0,0 +1,46 @@
+-- Create the database
+CREATE DATABASE todo;
+
+-- Change database
+USE todo;
+
+-- Create the Lists table
+CREATE TABLE Lists (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Create the Items table
+CREATE TABLE Items (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  list_id INT NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  is_completed BOOLEAN DEFAULT FALSE,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (list_id) REFERENCES Lists(id)
+);
+
+-- Create the Tags table
+CREATE TABLE Tags (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
+);
+
+-- Create the Item_Tags junction table
+CREATE TABLE Item_Tags (
+  item_id INT NOT NULL,
+  tag_id INT NOT NULL,
+  PRIMARY KEY (item_id, tag_id),
+  FOREIGN KEY (item_id) REFERENCES Items(id),
+  FOREIGN KEY (tag_id) REFERENCES Tags(id)
+);
+
+-- Create the Reminders table
+CREATE TABLE Reminders (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  item_id INT NOT NULL,
+  reminder_date DATE NOT NULL,
+  FOREIGN KEY (item_id) REFERENCES Items(id)
+);
