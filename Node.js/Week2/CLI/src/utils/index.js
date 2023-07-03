24e6bbb3cbf41180299049a71ea5a1f07f58754d
Darshana Sirisena <117305837+DarshanaSi@users.noreply.github.com>
2023-07-03 11:27:26 +0200

    Delete

--- Node.js/Week2/CLI/src/utils/index.js
+++
@@ -1,5 +0,0 @@
-export * from './getRequestData.js';
-export * from './requestHandler.js';
-export * from './fileHandler.js';
-export * from '../post/postHandler.js';
-export * from '../user/userHandler.js';
